7b21f6584f557a98221200e3e60caf7d6ec8b350
Bruno Nardini <bruno.nardini@m4u.com.br>
2020-04-19 20:10:26 -0300

    Add Storybook Addons: docs, viewport and knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
   ],
 };
