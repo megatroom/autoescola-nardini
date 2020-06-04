bdd169b95e3f97a661d080d9793528b6fc124228
Bruno Nardini <bruno.nardini@m4u.com.br>
2020-06-03 23:36:54 -0300

    Fix font-family

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,6 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 
 import { BreakpointSizes, breakAt } from "./Breakpoints";
 
@@ -81,4 +83,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
