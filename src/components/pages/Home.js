ce71dded03c0dfe6be22b2281eb38f7e26eb11fb
Bruno Nardini <bruno.nardini@m4u.com.br>
2020-09-07 16:56:43 -0300

    Fix video auto play

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -114,7 +114,14 @@ const Home = () => (
           </div>
         </div>
         <div>
-          <video src={AboutVideo} width="100%" autoPlay loop muted />
+          <video
+            src={AboutVideo}
+            width="100%"
+            autoPlay
+            playsInline
+            loop
+            muted
+          />
         </div>
       </Grid>
     </Section>
